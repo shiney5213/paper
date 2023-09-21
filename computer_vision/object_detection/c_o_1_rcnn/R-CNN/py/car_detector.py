78b37d3978b7d2685def986c6a12ba4343a70250
shiney5213 <shiney5213@gmail.com>
2023-09-21 13:23:51 +0900

    Test: test car and add commend

--- computer_vision/object_detection/c_o_1_rcnn/R-CNN/py/car_detector.py
+++ computer_vision/object_detection/c_o_1_rcnn/R-CNN/py/car_detector.py
@@ -42,7 +42,7 @@ def get_model(device=None):
     num_classes = 2
     num_features = model.classifier[6].in_features
     model.classifier[6] = nn.Linear(num_features, num_classes)
-    model.load_state_dict(torch.load('./models/best_linear_svm_alexnet_car.pth'))
+    model.load_state_dict(torch.load('./models/linear_svm/best_linear_svm_alexnet_car.pt'))
     model.eval()
 
     # 取消梯度追踪
@@ -56,25 +56,26 @@ def get_model(device=None):
 
 def draw_box_with_text(img, rect_list, score_list):
     """
-    绘制边框及其分类概率
+    경계 상자 및 분류 확률 그리기
     :param img:
     :param rect_list:
     :param score_list:
     :return:
     """
     for i in range(len(rect_list)):
-        xmin, ymin, xmax, ymax = rect_list[i]
-        score = score_list[i]
+        if i == 2:
+            xmin, ymin, xmax, ymax = rect_list[i]
+            score = score_list[i]
 
-        cv2.rectangle(img, (xmin, ymin), (xmax, ymax), color=(0, 0, 255), thickness=1)
-        cv2.putText(img, "{:.3f}".format(score), (xmin, ymin), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-
+            cv2.rectangle(img, (xmin, ymin), (xmax, ymax), color=(0, 0, 255), thickness=3)
+            cv2.putText(img, "{:.3f}".format(score), (xmin, ymin), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+            print('score', score)
 
 def nms(rect_list, score_list):
     """
-    非最大抑制
-    :param rect_list: list，大小为[N, 4]
-    :param score_list： list，大小为[N]
+    Non maximum suppression
+    :param rect_list: list，size[N, 4]
+    :param score_list： list，size[N]
     """
     nms_rects = list()
     nms_scores = list()
@@ -83,16 +84,17 @@ def nms(rect_list, score_list):
     score_array = np.array(score_list)
 
     # 一次排序后即可
-    # 按分类概率从大到小排序
+    # 분류 확률을 기준으로 내림차순 정렬
     idxs = np.argsort(score_array)[::-1]
     rect_array = rect_array[idxs]
     score_array = score_array[idxs]
 
     thresh = 0.3
     while len(score_array) > 0:
-        # 添加分类概率最大的边界框
+        # 가장 높은 positive 확률 저장
         nms_rects.append(rect_array[0])
         nms_scores.append(score_array[0])
+        # 나머지 확률과 상자 저장
         rect_array = rect_array[1:]
         score_array = score_array[1:]
 
@@ -100,10 +102,10 @@ def nms(rect_list, score_list):
         if length <= 0:
             break
 
-        # 计算IoU
+        # IoU 계산
         iou_scores = util.iou(np.array(nms_rects[len(nms_rects) - 1]), rect_array)
         # print(iou_scores)
-        # 去除重叠率大于等于thresh的边界框
+        # 겹치는 부분이 thresh보다 작으면 제거
         idxs = np.where(iou_scores < thresh)[0]
         rect_array = rect_array[idxs]
         score_array = score_array[idxs]
@@ -114,50 +116,80 @@ def nms(rect_list, score_list):
 if __name__ == '__main__':
     device = get_device()
     transform = get_transform()
-    model = get_model(device=device)
+    # load best_linear_svm
+    model = get_model(device=device)   
 
-    # 创建selectivesearch对象
+    # selectivesearch
     gs = selectivesearch.get_selective_search()
 
-    # test_img_path = '../imgs/000007.jpg'
-    # test_xml_path = '../imgs/000007.xml'
-    test_img_path = '../imgs/000012.jpg'
-    test_xml_path = '../imgs/000012.xml'
+    # 1) input data
+    test_img_path = './imgs/000007.jpg'
+    test_xml_path = './imgs/000007.xml'
+    # test_img_path = './imgs/000012.jpg'
+    # test_xml_path = './imgs/000012.xml'
 
     img = cv2.imread(test_img_path)
+    
     dst = copy.deepcopy(img)
+    selectivesearch_img = copy.deepcopy(img)
 
     bndboxs = util.parse_xml(test_xml_path)
     for bndbox in bndboxs:
         xmin, ymin, xmax, ymax = bndbox
-        cv2.rectangle(dst, (xmin, ymin), (xmax, ymax), color=(0, 255, 0), thickness=1)
+        cv2.rectangle(dst, (xmin, ymin), (xmax, ymax), color=(0, 255, 0), thickness=3)
 
-    # 候选区域建议
+    # 2) selective search
     selectivesearch.config(gs, img, strategy='f')
     rects = selectivesearch.get_rects(gs)
-    print('候选区域建议数目： %d' % len(rects))
+    print('제안된 후보 영역 수： %d' % len(rects))
+    
 
     # softmax = torch.softmax()
 
     svm_thresh = 0.60
 
-    # 保存正样本边界框以及
+    # save positive sample box 
     score_list = list()
     positive_list = list()
 
     # tmp_score_list = list()
     # tmp_positive_list = list()
     start = time.time()
-    for rect in rects:
+    for i, rect in enumerate(rects):
         xmin, ymin, xmax, ymax = rect
         rect_img = img[ymin:ymax, xmin:xmax]
-
+        
+       
+        
+        # transform size: (227, 227)
         rect_transform = transform(rect_img).to(device)
+        wrap_img = transform(rect_img)
+        
+        
+        # image visualize code
+        # width = xmax - xmin
+        # height = ymax - ymin
+        
+        # if width * height >= 3000:
+        #     cv2.rectangle(selectivesearch_img, (xmin, ymin), (xmax, ymax), color=(0, 0, 255), thickness=3)
+            
+        #     wrap_img = wrap_img.detach().cpu().numpy()
+            
+        #     c, w, h = wrap_img[0], wrap_img[1], wrap_img[2]
+        #     wrap_img = cv2.merge((h, w, c))
+        #     cv2.imshow('wrap img', wrap_img)
+        #     cv2.waitKey(0)
+            
+                
+        # model output: [ negative, positive ]
         output = model(rect_transform.unsqueeze(0))[0]
+        
 
+        # positive > negative
+        # argmax(): return index of big aixs
         if torch.argmax(output).item() == 1:
             """
-            预测为汽车
+            자동차에 대한 예측
             """
             probs = torch.softmax(output, dim=0).cpu().numpy()
 
@@ -168,10 +200,14 @@ if __name__ == '__main__':
                 score_list.append(probs[1])
                 positive_list.append(rect)
                 # cv2.rectangle(dst, (xmin, ymin), (xmax, ymax), color=(0, 0, 255), thickness=2)
-                print(rect, output, probs)
+                print(f'{i}th box:', rect, output, probs)
     end = time.time()
     print('detect time: %d s' % (end - start))
+    
+    print('positive_list', len(positive_list), positive_list[0])
+    print('score_list', len(score_list), score_list[0])
 
+    # raise ValueError
     # tmp_img2 = copy.deepcopy(dst)
     # draw_box_with_text(tmp_img2, tmp_positive_list, tmp_score_list)
     # cv2.imshow('tmp', tmp_img2)
@@ -181,8 +217,8 @@ if __name__ == '__main__':
     # cv2.imshow('tmp2', tmp_img)
 
     nms_rects, nms_scores = nms(positive_list, score_list)
-    print(nms_rects)
-    print(nms_scores)
+    print('nms_rects', len(nms_rects), nms_rects[0])
+    print('nms_scores', len(nms_scores), nms_scores[0])
     draw_box_with_text(dst, nms_rects, nms_scores)
 
     cv2.imshow('img', dst)
