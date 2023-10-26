b406f1b88c0f25b8d45f377a35622949f1b4dda0
shiney5213 <shiney5213@gmail.com>
2023-10-26 15:29:56 +0900

    feat : data preprocessing

--- computer_vision/classification/c_c_alexnet/dataloader.py
+++ computer_vision/classification/c_c_alexnet/dataloader.py
@@ -1,5 +1,7 @@
 # dataset download: https://academictorrents.com/collection/imagenet-2012
 
+import os
+import pandas as pd
 from torchvision import datasets 
 from torchvision import transforms
 from torch.utils import data
@@ -11,7 +13,39 @@ BATCH_SIZE = 128
 
 
 
-def dataloader():
+def datapreparation(TRAIN_IMG_DIR, TRAIN_ANNO_DIR):
+    length_annotations = len(os.listdir(TRAIN_ANNO_DIR))
+    length_image_classes = len(os.listdir(TRAIN_IMG_DIR)) 
+    
+    print('Length of annotations', length_annotations)
+    print('Length of image classes', length_image_classes)
+    
+    if length_annotations == length_image_classes:
+        print('Number of unique annotations matches the number of classes')
+    else:
+        print("Number of unique annotations doesn't the number of classes")
+    
+    valid = []
+    
+    for element in os.listdir(TRAIN_IMG_DIR):
+        breed = element.split('-')[1]
+        images = len(os.listdir(os.path.join(TRAIN_IMG_DIR, element)))
+        valid.append((breed, images))
+        
+    df = pd.DataFrame(valid, columns = ['Breed', 'Number of images'])
+    print('total number of images', df['Number of images'].sum())
+    
+    print(df.head())
+    
+    raise ValueError
+    return df
+
+
+
+
+def dataloader(TRAIN_IMG_DIR, TRAIN_ANNO_DIR):
+    
+    
     
     transform = transforms.Compose([
         # tranforms.RandomResizedCrop(IMAGE_DIM, scale = (0.9, 1.0), ratio = (0.9, 1.1)),
