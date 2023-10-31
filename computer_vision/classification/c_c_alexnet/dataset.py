db7f1e80c814ffd72ee8b302e1bec0b7e5a9a5d4
shiney5213 <shiney5213@gmail.com>
2023-10-31 16:26:48 +0900

    fix: StanfordDogs class

--- computer_vision/classification/c_c_alexnet/dataset.py
+++ computer_vision/classification/c_c_alexnet/dataset.py
@@ -2,20 +2,14 @@ import os
 import cv2
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 from torchvision import datasets 
 from torchvision import transforms
 from torch.utils import data
 from torch.utils.data import Dataset, DataLoader  
 from albumentations import RandomCrop, HorizontalFlip, CenterCrop, Compose, Normalize
 from albumentations.pytorch.transforms import ToTensor
-
-
 from datapreprocessing import PCA_color_aug
-
-
-TRAIN_IMG_DIR = './alexnet_data_in/imagenet'
-IMAGE_DIM = 256
-BATCH_SIZE = 128
 
 
 
@@ -39,8 +33,7 @@ def prepare_dataset(TRAIN_IMG_DIR, TRAIN_ANNO_DIR):
     classes = select_classes(label_num_df, 5)
     path_label_df = select_path_label(TRAIN_IMG_DIR, classes )
     
-    
-    return  classes, path_label_df
+    return  classes, path_label_df, label_num_df
 
 def create_label_num(TRAIN_IMG_DIR):
     """
@@ -56,7 +49,6 @@ def create_label_num(TRAIN_IMG_DIR):
     df = pd.DataFrame(valid, columns = ['Breeds', 'Number of images'])
     df = df.sort_values('Number of images', ascending = False)
     print('total number of images', df['Number of images'].sum())
-    print(df.head())
     
     return df
 
@@ -65,22 +57,22 @@ def select_classes(df, class_num):
     select the 5 classes with the largest number: sebset
     '''
     classes = df.sort_values('Number of images', ascending = False)['Breeds'][:class_num].values.tolist()
-    print('subset', classes)
+    print('classes', classes)
     
     return classes
 
 
-def select_path_label(TRAIN_IMG_DIR, subset ):
+def select_path_label(TRAIN_IMG_DIR, classes ):
     """
     create df with path and label
     """
     valid = []
     for element in os.listdir(TRAIN_IMG_DIR):
-        if element.split('-')[1] in subset:
+        if element.split('-')[1] in classes:
             element_path = os.listdir(os.path.join(TRAIN_IMG_DIR, element))
             for img_id in element_path:
                 path = os.path.join(element, img_id)
-                label = element.split('_')
+                label = element.split('_')[0].split('-')[1]
                 valid.append((path, label))
                 
     df = pd.DataFrame(valid, columns = ['Path', 'Label'])
@@ -89,15 +81,19 @@ def select_path_label(TRAIN_IMG_DIR, subset ):
 
 
 class StanfordDogs(Dataset):
-    def __init__(self, transform1, transform2, X, Y, image_path, objective = 'train'):
+    def __init__(self, transform1, transform2, X, Y, image_path,  objective, is_show = False):
+    # def __init__(self, X, Y, image_path,  objective, is_show = False):
+        
         self.X = X
         self.Y = Y
-        self.train_trainform = transform1
-        self.valid_transform = transform2
+        # self.train_trainform = transform1
+        # self.valid_transform = transform2
         self.objective = objective
         self.image_path = image_path
         self.IMAGE_TRAIN_DIM = 256
-        self.IMAGE_TEST_DIM = 244
+        self.IMAGE_TEST_DIM = 224
+        self.is_show = is_show
+        
         
 
     def __getitem__(self, idx):
@@ -105,59 +101,118 @@ class StanfordDogs(Dataset):
         label = self.Y.iloc[idx, :].values
         img = cv2.imread(os.path.join(self.image_path, path))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = self.image_resize()
-        img = self.is_train(img)
         
-        return img, label
+        img_resize_256, img_centercrop = self.image_resize_256(img)
+        
+        img_pac_aug, img_final = self.train_aug(img_centercrop)
+            
+        
+        if self.is_show:
+            self.show_img('Original Image', img)
+            print('ordinary image', img.shape)
+            self.show_img('resize_256', img_resize_256)
+            self.show_img('img_centercrop', img_centercrop)
+            print('resize_256 image', img_resize_256.shape)
+            print('img_centercrop image', img_centercrop.shape)
+            self.show_img('PCA Color Augmentation', img_pac_aug)
+            print('img_pca_aug', img_pac_aug.shape)
+            self.show_img('transform', img_final )
+            print('img_transform', img_final)
+                
+        
+            
+        return img_final, label
+    
+    def show_img(self, img_name, img):
+        cv2.imshow( img_name, img)
+        cv2.waitKey(0)
+        
+        
         
         
     def __len__(self):
         return len(self.X)
     
-    def image_resize(self):
-                # Shortest side of image is scaled to 256 pixels and the other side is scaled so as to maintain aspect ratio
+    def image_resize_256(self, img):
+        # Shortest side of image is scaled to 256 pixels and the other side is scaled so as to maintain aspect ratio
         h, w, _ = img.shape
         
         if h <= w:
             aspect_ratio = w/h
             dim = (self.IMAGE_TRAIN_DIM, int(self.IMAGE_TRAIN_DIM * aspect_ratio))
-            img = cv2.resize(img, dim)
+            img_256 = cv2.resize(img, dim)
         else:
-            aspect = h/w
+            aspect_ratio = h/w
             dim = (int(self.IMAGE_TRAIN_DIM * aspect_ratio), self.IMAGE_TRAIN_DIM)
-            img = cv2.resize(img, dim)
-            
-        img = CenterCrop(height = self.IMAGE_TRAIN_DIM, width = self.IMAGE_TRAIN_DIM, p = 1)(image = img)['image']
+            img_256 = cv2.resize(img, dim)
         
-        return img
+        img_centercrop = CenterCrop(height = self.IMAGE_TRAIN_DIM, width = self.IMAGE_TRAIN_DIM, p = 1)(image = img_256)['image']
+        
+        
+        return img_256, img_centercrop
     
-    def is_train(self, img):
+    def train_aug(self, img):
         if self.objective == 'train':
             random = np.random.uniform(size = 1)
+            # PCA Augmentation carried out only 50 percent of time
             if random < 0.5:
-                img = PCA_color_aug(img, category = 'numpy')
-            augmented = self.train_trainform(image = img)
+                img_pac_aug = PCA_color_aug(img, category = 'numpy')
+            augmented = self.train_transform(image = img_pac_aug)
             
         elif ((self.objective == 'validation') | (self.objective == 'test')):
-            img = cv2.resize(img, (self.IMAGE_TEST_DIM, self.IMAGE_TEST_DIM))
-            augmented = self.valid_transform(image = img)
+            img_resize_224 = cv2.resize(img, (self.IMAGE_TEST_DIM, self.IMAGE_TEST_DIM))
+            augmented = self.valid_transform(image = img_resize_224)
+            img_pac_aug = img_resize_224
             
-        return augmented['image']
-            
-            
-        
-def augmentation1():
-    return Compose([RandomCrop(height = IMAGE_DIM, widht = IMAGE_DIM, p = 1.0),
+        return img_pac_aug, augmented['image']
+    
+    def normalise(self):
+        return dict(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])
+    
+    def train_transform(self):
+        transform = Compose([RandomCrop(height = self.IMAGE_TEST_DIM, width = self.IMAGE_TEST_DIM, p = 1.0),
                     HorizontalFlip(p = 0.5),
-                    ToTensor(Normalize = normalise)],
+                    ToTensor(normalize= self.normalise)],
                     p = 1   # the exponent value in the norm formulation.
                     )
-    
-def augmentation2():
-    return Compose([ToTensor(normalize = normalise)],
+        
+        return transform
+        
+        
+        
+    def valid_transform(self):
+        return Compose([ToTensor(normalize = self.normalise)],
                    p = 1)
             
+        
+    
+    
 
-def normalise():
-    return dict(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])
-
+            
+            
+def get_dataset(df):
+   
+    labels = pd.get_dummies(df['Label'])
+    train_X, test_X, train_y, test_y = train_test_split(df, 
+                                                        labels, 
+                                                        test_size = 0.25,
+                                                        random_state = 5, 
+                                                        stratify = df['Label']
+                                                        )
+    
+    train_X, val_X, train_y, val_y = train_test_split(train_X,
+                                                      train_y,
+                                                      test_size = 0.25,
+                                                      random_state = 5,
+                                                      stratify = train_X['Label'])
+    
+    train_X.reset_index(drop = True, inplace = True)
+    val_X.reset_index(drop = True, inplace = True)
+    test_X.reset_index(drop = True, inplace = True)
+    
+    train_y.reset_index(drop = True, inplace = True)
+    val_y.reset_index(drop = True, inplace = True)
+    test_y.reset_index(drop = True, inplace = True)
+    
+    return train_X, val_X, test_X, train_y, val_y, test_y
+    
