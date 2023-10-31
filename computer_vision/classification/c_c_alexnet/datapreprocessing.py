b40841b71c731ac86c3e01b4d79ff4106ccc57c2
shiney5213 <shiney5213@gmail.com>
2023-10-31 16:26:24 +0900

    feat: change pca_color_aug()

--- computer_vision/classification/c_c_alexnet/datapreprocessing.py
+++ computer_vision/classification/c_c_alexnet/datapreprocessing.py
@@ -6,11 +6,33 @@ import torch
 from torchvision import datasets 
 from torchvision import transforms
 from torch.utils import data
+from albumentations import RandomCrop, HorizontalFlip, CenterCrop, Compose, Normalize
+from albumentations.pytorch.transforms import ToTensor
+# from torchvision.transforms import Compose, RandomCrop, ToTensor, Normalize, RandomHorizontalFlip
+
 
 
 TRAIN_IMG_DIR = './alexnet_data_in/imagenet'
-IMAGE_DIM = 256
+IMAGE_TRAIN_DIM = 256
+IMAGE_TEST_DIM = 224
 BATCH_SIZE = 128
+
+
+def augmentation1():
+    return Compose([RandomCrop(height = IMAGE_TEST_DIM, width = IMAGE_TEST_DIM, p = 1.0),
+                    HorizontalFlip(p = 0.5),
+                    ToTensor(normalize= normalise)],
+                    p = 1   # the exponent value in the norm formulation.
+                    )
+    
+def augmentation2():
+    return Compose([ToTensor(normalize = normalise)],
+                   p = 1)
+            
+
+def normalise():
+    return dict(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])
+
 
 
 def PCA_color_aug(image, category = 'Tensor'):
@@ -23,7 +45,7 @@ def PCA_color_aug(image, category = 'Tensor'):
     
     if type(image)==torch.Tensor:
         image = image.numpy()
-        image - np.moveaxis(image, 0, 2)
+        image = np.moveaxis(image, 0, 2)
         
     img_reshaped = image.reshape(-1, 3).astype('float32')
     mean, std = np.mean(img_reshaped, 0), np.std(img_reshaped, 0)
@@ -36,9 +58,10 @@ def PCA_color_aug(image, category = 'Tensor'):
     valid = np.dot(eigen_vec, vec1)
     pca_aug_norm_image = img_rescaled + valid
     pca_aug_image = pca_aug_norm_image * std + mean
-    aug_image = np.maximum(np.minimum(pca_aug_image, 255), 0).astype('unit8')
-    if category == ' Tensot':
-        return torch.from_numpy(aug_image.reshape(3, IMAGE_DIM, IMAGE_DIM))
+    print('pca_aug_image:', pca_aug_image.shape, pca_aug_image[0,0], type( pca_aug_image[0,0]))
+    aug_image = np.maximum(np.minimum(pca_aug_image, 255), 0).astype(np.uint8)
+    if category == 'Tensor':
+        return torch.from_numpy(aug_image.reshape(3, IMAGE_TRAIN_DIM, IMAGE_TRAIN_DIM))
     else:
-        return aug_image.reshape(IMAGE_DIM,IMAGE_DIM,3)
+        return aug_image.reshape(IMAGE_TRAIN_DIM,IMAGE_TRAIN_DIM,3)
     
