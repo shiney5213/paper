4fa5ac7550bb5de29b66f38a3f1dd8753bbf5925
shiney5213 <shiney5213@gmail.com>
2023-11-08 17:11:56 +0900

    feat: model_save()

--- computer_vision/classification/c_c_alexnet/utils.py
+++ computer_vision/classification/c_c_alexnet/utils.py
@@ -1,13 +1,8 @@
 import numpy as np
 import cv2
+import os
 from sklearn.metrics import accuracy_score
-
-
-
-    
-    
-import matplotlib.pyplot as plt
-
+import torch
 
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
@@ -19,10 +14,21 @@ def acculate(y, output):
     return acc
     
     
+def model_save(CHECKPOINT_DIR, model, optimizer, losses_df,  seed, epoch, lr_list):
     
-def historyplot(losses_df):
+    checkpoint_path = os.path.join(CHECKPOINT_DIR, 'alexnet_states_e{}.pth'.format(epoch + 1))
+        
+    state = {
+        'epoch': epoch,
+        'optimizer': optimizer.state_dict(),
+        'model' : model.state_dict(),
+        'seed' : seed,
+        'losses_df': losses_df,
+        'lr': lr_list
+    }
     
-    val_loss = losses_df['val_loss']
-    
-    plt.plot(val_loss)
+    torch.save(state, checkpoint_path)
+    print('model save')
+       
+        
     
